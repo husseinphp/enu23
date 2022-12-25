3f6cfc723df5244185a06afc9ecb1c246b6dea83
Hussein Ali Taha <69895843+husseinphp@users.noreply.github.com>
2022-12-25 14:23:39 +0300

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
 alias amass='docker run -it --rm -w /data -v $(pwd):/data caffix/amass'
+sleep 2
 amass enum -passive -norecursive -noalts –d $1 -o $1sub.txt
+sleep 2
+
 telify -f $1sub.txt
